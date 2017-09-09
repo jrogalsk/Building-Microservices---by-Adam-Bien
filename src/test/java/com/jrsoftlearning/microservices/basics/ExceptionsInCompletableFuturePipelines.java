74674c130b81eac6da481f574063621f179d69bd
Jan Rogalski <janrogalski@Mac-mini.local>
2017-09-10 00:03:52 +0200

    14. Handling exceptions in Completable Future

---
+++ src/test/java/com/jrsoftlearning/microservices/basics/ExceptionsInCompletableFuturePipelines.java
@@ -0,0 +1,36 @@
+package com.jrsoftlearning.microservices.basics;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static java.lang.System.out;
+
+public class ExceptionsInCompletableFuturePipelines {
+
+    @Test
+    public void handle() throws ExecutionException, InterruptedException {
+        CompletableFuture.supplyAsync(this::exceptional)
+                .exceptionally(this::handleException)
+                .thenAccept(this::consume)
+                .get();
+    }
+
+
+    private String exceptional() {
+        throw new IllegalStateException("happens");
+    }
+
+
+    String handleException(Throwable t) {
+        return t.toString();
+    }
+
+    void consume(String message) {
+        out.println("messaeg = " + message);
+    }
+
+
+
+}
