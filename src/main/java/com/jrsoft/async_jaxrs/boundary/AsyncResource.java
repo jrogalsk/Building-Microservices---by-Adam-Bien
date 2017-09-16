7d6a12b83ed456181691eb72f5a1f27bf497c8b7
Jan Rogalski <janrogalski@Mac-mini.local>
2017-09-16 21:21:48 +0200

    28. Serverside service orchestration - calling one microservice from another

--- src/main/java/com/jrsoft/async_jaxrs/boundary/AsyncResource.java
+++ src/main/java/com/jrsoft/async_jaxrs/boundary/AsyncResource.java
@@ -1,12 +1,21 @@
 package com.jrsoft.async_jaxrs.boundary;
 
+import org.glassfish.jersey.client.ClientProperties;
+
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.Response;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,6 +24,46 @@ public class AsyncResource {
 
     @Resource
     ManagedExecutorService mes;
+
+    private Client client;
+    private WebTarget tut;
+    private WebTarget processor;
+
+    @PostConstruct
+    public void init() {
+        this.client = ClientBuilder.newClient();
+        this.client.property(ClientProperties.CONNECT_TIMEOUT, 1000);
+        this.client.property(ClientProperties.READ_TIMEOUT, 5000);
+        this.tut = this.client.target("http://localhost:8080/suplier/resources/messages");
+        this.processor = this.client.target("http://localhost:8080/suplier/resources/processors/beautification");
+    }
+
+    @GET
+    @Path("orchestration")
+    public String fetchMessage() throws InterruptedException, java.util.concurrent.ExecutionException {
+        Supplier<String> messageSupplier = () -> this.tut.request().get(String.class);
+        CompletableFuture
+                .supplyAsync(messageSupplier, mes)
+                .thenApply(this::process)
+                .exceptionally(this::handle)
+                .thenAccept(this::consume)
+                .get();
+
+        return "+++";
+    }
+
+    private String handle(Throwable throwable) {
+        return String.format("Sorry we are overloaded: %s", throwable.toString());
+    }
+
+    String process(String input) {
+        Response response =  this.processor.request().post(Entity.text(input));
+        return response.readEntity(String.class);
+    }
+
+    private void consume(String message) {
+        this.tut.request().post(Entity.text(message));
+    }
 
     @GET
     public void get(@Suspended AsyncResponse response) {
